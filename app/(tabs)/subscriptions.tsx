570d8dbad1a4051e7df10979c3119577138541e6
MEMOIIV <ameensaid80@gmail.com>
2026-08-10 15:36:14 +0300

    implement fixes

--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -1,7 +1,7 @@
 import { Link } from "expo-router";
 import { styled } from "nativewind";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 const SafeAreaView = styled(RNSafeAreaView);
 const subscriptions = () => {
