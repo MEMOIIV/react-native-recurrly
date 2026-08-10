e352124ceaa885f610c52625071047654c201af1
MEMOIIV <ameensaid80@gmail.com>
2026-08-10 15:01:38 +0300

    first project setup nativewind, routing, tab navigation

--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -1,10 +1,12 @@
 import { Link } from "expo-router";
+import { styled } from "nativewind";
 import React from "react";
 import { Text, View } from "react-native";
-
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
 const subscriptions = () => {
   return (
-    <View className="p-8 mt-4">
+    <SafeAreaView className="flex-1  bg-background p-5">
       <Text>subscriptions</Text>
       <Link
         href="/subscriptions/spotify"
@@ -21,7 +23,7 @@ const subscriptions = () => {
       >
         Claude Subscription
       </Link>
-    </View>
+    </SafeAreaView>
   );
 };
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,20 @@
 import "@/global.css";
 import { Stack } from "expo-router";
-import { useFonts } from "expo-font";
+// import { useFonts } from "expo-font";
 import { View } from "react-native";
 
 export default function RootLayout() {
-  const [fontsLoaded] = useFonts({
-    CairoRegular: require("../assets/fonts/Cairo-Regular.ttf"),
-    CairoLight: require("../assets/fonts/Cairo-Light.ttf"),
-    CairoMedium: require("../assets/fonts/Cairo-Medium.ttf"),
-    CairoSemiBold: require("../assets/fonts/Cairo-SemiBold.ttf"),
-    CairoBold: require("../assets/fonts/Cairo-Bold.ttf"),
-  });
+  // const [fontsLoaded] = useFonts({
+  //   CairoRegular: require("../assets/fonts/Cairo-Regular.ttf"),
+  //   CairoLight: require("../assets/fonts/Cairo-Light.ttf"),
+  //   CairoMedium: require("../assets/fonts/Cairo-Medium.ttf"),
+  //   CairoSemiBold: require("../assets/fonts/Cairo-SemiBold.ttf"),
+  //   CairoBold: require("../assets/fonts/Cairo-Bold.ttf"),
+  // });
 
-  if (!fontsLoaded) {
-    return <View />;
-  }
+  // if (!fontsLoaded) {
+  //   return <View />;
+  // }
 
   return <Stack screenOptions={{headerShown:false}} />;
 }
